3ca0eb79640b0fa446407a471d00b00341cb09d7
geek14 <aloshkjosejabar@gmail.com>
2017-11-16 10:36:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notificationdemo/NotificationView.java
@@ -0,0 +1,19 @@
+package com.example.notificationdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by geek14 on 16-11-2017.
+ */
+
+public class NotificationView extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
